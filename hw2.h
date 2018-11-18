8dfd221e6058c558d300eb9b49133de860618a87
ShaniBarzel <shanibarzel@campus.technion.ac.il>
2018-11-18 19:19:47 +0200

    compure first

--- hw2.h
+++ hw2.h
@@ -17,42 +17,7 @@ bool ContainsTerminals(std::vector<int> tavnit){
     it++;
 }
 
-
-/**
-*** CheckNullRule by Shani and Racheli
- * input: Grammer Rule
- * output: true if the rule is "nullable" = the variable in the right section of the rule, can derive epsilon by using this rule
- */
- bool CheckNullRule(grammar_rule Gr){
-    if (Gr.rhs().empty())
-        return true; //the rule has only epsilon in the right section
-
-    if (ContainsTerminals(Gr.rhs()))
-        return false; //the rule contains terminal in the right section (so the rule is not nullable)
-
-    //if we reached here, the rule contains only non-terminals in the right section
-    int  is_null = 0; //counter of the nullable non-terminals in the right section of the rule
-
-    //iterate all the non-terminals in the right section of the rule
-    for ( std::vector<int>::const_iterator R_it = Gr.rhs().begin(); R_it != Gr.rhs.end(); R_it++){
-        // iterate all the rules
-        for(std::vector<grammar_rule>::const_iterator Rules_it = grammar.begin(); Rules_it != grammar.end(); Rules_it++){
-            // if the rule left section is the current non-terminal from the right section of Gr, check if it is nullable
-            if ( ((*Rules_it).lhs == *R_it) && (Gr != *Rules_it) ) { //second condition is for avoiding endless recursion
-                if (CheckNullRule(*Rules_it)) {
-                    is_null++; //the current non-terminal of the right section of Gr is nullable
-                    break; //(from for loop)
-                } //if the current rule is not nullable, but other rule is nullable, the non-terminal is nullable
-            }
-        }
-    }
-    if (is_null == Gr.rhs().size()) //all the non-terminals in the right section of Gr are nullable
-        return true;
-}
-
-
-/** =================== Fixed Point Version
-*** CheckNullRule by Shani and Racheli
+/*** CheckNullRule by Shani and Racheli
  * input: Grammer Rule and a vector that contains for each non terminal if it is nullable
  * output: true if the rule is "nullable" = the variable in the right section of the rule, can derive epsilon by using this rule
  */
@@ -73,44 +38,16 @@ bool CheckNullRule(grammar_rule Gr, std::vector<bool> IsNonterminalsNullable){
     return true;
 }
 
-/** ======================================== GIVEN FUNCTION STARTS HERE =============================================**/
 
 /**
- * determines which variables are nullable, i.e. can produce an empty word
- * calls print_nullable when finished
+ * input: nonterminal
+ * output: if the nonterminal is nullable
  */
-void compute_nullable(){
-    std::vector<bool> IsNonterminalsNullable(); //answer vector
-    int nonterminal = S;
-    // iterate all the nonterminals
-    while(nonterminal != NONTERMINAL_ENUM_SIZE){
-        // iterate all the rules
-        for (std::vector<grammar_rule>::const_iterator it = grammar.begin(); it != grammar.end(); it++) {
-            if ( ((*it).lhs == nonterminal) && (CheckNullRule(*it))) {
-                // if the currnet rule starts with the currnet nonterminal and the rule is nullable, then the current nonterminal is nullable
-                IsNonterminalsNullable().insert(true);
-                break; // (from for loop)
-            }
-        }
-        if (IsNonterminalsNullable().size() < nonterminal){
-            // if no bool value added to the answer vector for the currnt nonterminal, then non of the rules that starts with it are nullable
-            IsNonterminalsNullable().insert(false);
-        }
-        nonterminal++;
-    }
-    print_nullable(IsNonterminalsNullable);
-}
-
-/** =================== Fixed Point Version
-/**
- * determines which variables are nullable, i.e. can produce an empty word
- * calls print_nullable when finished
- */
-void compute_nullable(){
+bool IsNullable(int NonTerm){
     std::vector<bool> IsNonterminalsNullable(); //answer vector
     //init answer vector to false
     for (int i=0; i< NONTERMINAL_ENUM_SIZE; i++){
-        IsNonterminalsNullable.insert(false);
+        IsNonterminalsNullable().insert(false);
     }
 
     bool nothing_changed = true;
@@ -132,7 +69,7 @@ void compute_nullable(){
             }
         }
 
-        if (IsNonterminalsNullable[nonterminal] == true){
+        if (IsNonterminalsNullable()[nonterminal] == true){
             //no need to check the non-terminal, we already know it is nullable
             nonterminal++;
             continue;
@@ -140,9 +77,9 @@ void compute_nullable(){
 
         // iterate over all the rules
         for (std::vector<grammar_rule>::const_iterator it = grammar.begin(); it != grammar.end(); it++) {
-            if ( ((*it).lhs == nonterminal) && (CheckNullRule(*it,IsNonterminalsNullable))) {
+            if ( ((*it).lhs == nonterminal) && (CheckNullRule(*it,IsNonterminalsNullable()))) {
                 // if the currnet rule starts with the currnet nonterminal and the rule is nullable, then the current nonterminal is nullable
-                IsNonterminalsNullable[nonterminal] = true;
+                IsNonterminalsNullable()[nonterminal] = true;
                 nothing_changed = false; //if we reached here, than the current non-terminal was not nullable
                 break; // (from for loop)
             }
@@ -150,14 +87,119 @@ void compute_nullable(){
 
         nonterminal++;
     }
-    print_nullable(IsNonterminalsNullable);
+    return IsNonterminalsNullable()[nonterminal];
+}
+
+/** ======================================== GIVEN FUNCTION STARTS HERE =============================================**/
+
+/**
+ * determines which variables are nullable, i.e. can produce an empty word
+ * calls print_nullable when finished
+ */
+void compute_nullable(){
+    std::vector<bool> IsNonterminalsNullable(); //answer vector
+    //init answer vector to false
+    for (int i=0; i< NONTERMINAL_ENUM_SIZE; i++){
+        IsNonterminalsNullable().insert(false);
+    }
+
+    bool nothing_changed = true;
+    bool done = false;
+    int nonterminal = S;
+    // iterate all the nonterminals, while there is still changes
+    while(!done){
+        //if we are done iterating over all the non-terminlas
+        if (nonterminal == NONTERMINAL_ENUM_SIZE){
+            if (nothing_changed == true){
+                //no changes has done compering to last iteration over all the non-terminal - no more updates
+                done = true;
+                continue;
+            }
+            else{
+                //there was some changes compering to the last iteration, start itarating over all the non terminal once again
+                nothing_changed = true;
+                nonterminal = S;
+            }
+        }
+
+        if (IsNonterminalsNullable()[nonterminal] == true){
+            //no need to check the non-terminal, we already know it is nullable
+            nonterminal++;
+            continue;
+        }
+
+        // iterate over all the rules
+        for (std::vector<grammar_rule>::const_iterator it = grammar.begin(); it != grammar.end(); it++) {
+            if ( ((*it).lhs == nonterminal) && (CheckNullRule(*it,IsNonterminalsNullable()))) {
+                // if the currnet rule starts with the currnet nonterminal and the rule is nullable, then the current nonterminal is nullable
+                IsNonterminalsNullable()[nonterminal] = true;
+                nothing_changed = false; //if we reached here, than the current non-terminal was not nullable
+                break; // (from for loop)
+            }
+        }
+
+        nonterminal++;
+    }
+    print_nullable(IsNonterminalsNullable());
 }
 
 /**
  * computes first for all nonterminal (see nonterminal enum in grammar.h)
  * calls print_first when finished
  */
-void compute_first();
+void compute_first(){
+    std::vector<std::set<tokens >>& vec();
+    vec().reserve(EF); //init vector to the size of terminals + non_terminals
+    //init all the first's of the tokens to contain themselves
+    for (int token = KEY; token <EF; token++){
+        vec()[token].insert(token);
+    }
+
+    bool nothing_changed = true;
+    bool done = false;
+    int nonterminal = S;
+    // iterate all the nonterminals, while there is still changes
+    while(!done){
+        //if we are done iterating over all the non-terminlas
+        if (nonterminal == NONTERMINAL_ENUM_SIZE){
+            if (nothing_changed == true){
+                //no changes has done compering to last iteration over all the non-terminal - no more updates
+                done = true;
+                continue;
+            }
+            else{
+                //there was some changes compering to the last iteration, start itarating over all the non terminal once again
+                nothing_changed = true;
+                nonterminal = S;
+            }
+        }
+
+        // iterates over all the rules
+        for (std::vector<grammar_rule>::const_iterator it = grammar.begin(); it != grammar.end(); it++) {
+            if  ((*it).lhs == nonterminal) {
+                // if the currnet rule left section is the currnet nonterminal
+                for(std::vector<int>::const_iterator R_it = (*it).rhs.begin(); R_it!=(*it).rhs.end(); R_it++) {
+                    int current_first_size = vec()[nonterminal].size();
+                    //unite the "firsts" the nonterminals
+                     std::set_union(vec()[nonterminal].begin(), vec()[nonterminal].end() ,vec()[*R_it].begin(),
+                          vec()[*R_it].end(),std::inserter(vec()[nonterminal],vec()[nonterminal].begin()));
+
+                     int new_first_size = vec()[nonterminal].size();
+
+                     if (new_first_size != current_first_size)
+                         nothing_changed = false; //if we reached here, than the current non-terminal first's was updated
+
+                     // if the current variable is not nullable, the rest of the right secion variables is irrelevent
+                     if (!IsNullable(*R_it))
+                         break; // (from inner for)
+                }
+            }
+        }
+
+        nonterminal++;
+    }
+}
+
 
 /**
  * computes follow for all nonterminal (see nonterminal enum in grammar.h)

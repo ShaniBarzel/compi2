977f0bde5d6a328a6aae8cf9a40f439b20f91372
ShaniBarzel <shanibarzel@campus.technion.ac.il>
2018-11-20 17:34:35 +0200

    compure first

--- hw2.h
+++ hw2.h
@@ -40,10 +40,10 @@ bool CheckNullRule(grammar_rule Gr, std::vector<bool> IsNonterminalsNullable){
 
 
 /**
- * input: nonterminal
- * output: if the nonterminal is nullable
+ * input: -
+ * output: a vector that shows for each non terminal if it is nullable
  */
-bool IsNullable(int NonTerm){
+std::vector<bool> IsNullableNon(){
     std::vector<bool> IsNonterminalsNullable(); //answer vector
     //init answer vector to false
     for (int i=0; i< NONTERMINAL_ENUM_SIZE; i++){
@@ -87,67 +87,15 @@ bool IsNullable(int NonTerm){
 
         nonterminal++;
     }
-    return IsNonterminalsNullable()[nonterminal];
+    return IsNonterminalsNullable();
 }
 
-/** ======================================== GIVEN FUNCTION STARTS HERE =============================================**/
 
 /**
- * determines which variables are nullable, i.e. can produce an empty word
- * calls print_nullable when finished
+ * input: -
+ * output: a vector, containing the first of all variabls
  */
-void compute_nullable(){
-    std::vector<bool> IsNonterminalsNullable(); //answer vector
-    //init answer vector to false
-    for (int i=0; i< NONTERMINAL_ENUM_SIZE; i++){
-        IsNonterminalsNullable().insert(false);
-    }
-
-    bool nothing_changed = true;
-    bool done = false;
-    int nonterminal = S;
-    // iterate all the nonterminals, while there is still changes
-    while(!done){
-        //if we are done iterating over all the non-terminlas
-        if (nonterminal == NONTERMINAL_ENUM_SIZE){
-            if (nothing_changed == true){
-                //no changes has done compering to last iteration over all the non-terminal - no more updates
-                done = true;
-                continue;
-            }
-            else{
-                //there was some changes compering to the last iteration, start itarating over all the non terminal once again
-                nothing_changed = true;
-                nonterminal = S;
-            }
-        }
-
-        if (IsNonterminalsNullable()[nonterminal] == true){
-            //no need to check the non-terminal, we already know it is nullable
-            nonterminal++;
-            continue;
-        }
-
-        // iterate over all the rules
-        for (std::vector<grammar_rule>::const_iterator it = grammar.begin(); it != grammar.end(); it++) {
-            if ( ((*it).lhs == nonterminal) && (CheckNullRule(*it,IsNonterminalsNullable()))) {
-                // if the currnet rule starts with the currnet nonterminal and the rule is nullable, then the current nonterminal is nullable
-                IsNonterminalsNullable()[nonterminal] = true;
-                nothing_changed = false; //if we reached here, than the current non-terminal was not nullable
-                break; // (from for loop)
-            }
-        }
-
-        nonterminal++;
-    }
-    print_nullable(IsNonterminalsNullable());
-}
-
-/**
- * computes first for all nonterminal (see nonterminal enum in grammar.h)
- * calls print_first when finished
- */
-void compute_first(){
+std::vector<std::set<tokens>> First(){
     std::vector<std::set<tokens >>& vec();
     vec().reserve(EF); //init vector to the size of terminals + non_terminals
     //init all the first's of the tokens to contain themselves
@@ -181,23 +129,77 @@ void compute_first(){
                 for(std::vector<int>::const_iterator R_it = (*it).rhs.begin(); R_it!=(*it).rhs.end(); R_it++) {
                     int current_first_size = vec()[nonterminal].size();
                     //unite the "firsts" the nonterminals
-                     std::set_union(vec()[nonterminal].begin(), vec()[nonterminal].end() ,vec()[*R_it].begin(),
-                          vec()[*R_it].end(),std::inserter(vec()[nonterminal],vec()[nonterminal].begin()));
+                    std::set_union(vec()[nonterminal].begin(), vec()[nonterminal].end() ,vec()[*R_it].begin(),
+                                   vec()[*R_it].end(),std::inserter(vec()[nonterminal],vec()[nonterminal].begin()));
 
-                     int new_first_size = vec()[nonterminal].size();
+                    int new_first_size = vec()[nonterminal].size();
 
-                     if (new_first_size != current_first_size)
-                         nothing_changed = false; //if we reached here, than the current non-terminal first's was updated
+                    if (new_first_size != current_first_size)
+                        nothing_changed = false; //if we reached here, than the current non-terminal first's was updated
 
-                     // if the current variable is not nullable, the rest of the right secion variables is irrelevent
-                     if (!IsNullable(*R_it))
-                         break; // (from inner for)
+                    // if the current variable is not nullable, the rest of the right secion variables is irrelevent
+                    if (!IsNullableNon(*R_it))
+                        break; // (from inner for)
                 }
             }
         }
 
         nonterminal++;
     }
+    return vec();
+}
+
+
+/**
+ * input: a clause as int vector
+ * output: the clause "first"
+ */
+std::set<tokens> ClauseFirst(std::vector<int> clause){
+    std::set<tokens> first(); // return vactor
+    std::vector<std::set<tokens>> VarFirst = First();
+    for(int i = 0; i<clause.size(); i++) {
+        //for all terminals and non terminals in the clause, unite them with the current First of the clause
+        std::set_union(first().begin(), first().end() ,VarFirst[i].begin(),
+                       VarFirst()[i].end(),std::inserter(first(),first().begin()));
+    }
+    return first();
+}
+
+
+/**
+ * input: a clause as int vector
+ * output: true if the clause is nullable
+ */
+bool IsNullableClause(std::vector<int> clause){
+    std::vector<bool> IsNull = IsNullableNon();
+    for(int i = 0; i<clause.size(); i++) {
+        //if one of the variables in the clause is nullable, the clause is not nullable
+        if (i>NONTERMINAL_ENUM_SIZE || !IsNull[i])
+            return false;
+    }
+    return true;
+}
+
+
+/** ======================================== GIVEN FUNCTION STARTS HERE =============================================**/
+
+/**
+ * determines which variables are nullable, i.e. can produce an empty word
+ * calls print_nullable when finished
+ */
+void compute_nullable(){
+    std::vector<bool> IsNonterminalsNullable = IsNullableNon(); //answer vector
+    //init answer vector to false
+    print_nullable(IsNonterminalsNullable());
+}
+
+/**
+ * computes first for all nonterminal (see nonterminal enum in grammar.h)
+ * calls print_first when finished
+ */
+void compute_first(){
+    std::vector<std::set<tokens >>& vec() = First();
+    print_first(vec());
 }
 
 
@@ -211,8 +213,14 @@ void compute_follow();
  * computes select for all grammar rules (see grammar global variable in grammar.h)
  * calls print_select when finished
  */
-void compute_select();
+void compute_select(){
+    std::vector<std::set<tokens> >& vec();
+    vec().reserve(grammar.size());
 
+    for (std::vector<grammar_rule>::const_iterator it = grammar.begin(); it != grammar.end(); it++) {
+
+    }
+}
 /**
  * implements an LL(1) parser for the grammar using yylex()
  */
